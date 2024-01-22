cb1bf1139b5cacc68ea1c530a03aa255952b3dba
Aakash <aakash_shakya@hotmail.com>
2024-01-22 23:30:05 +0545

    Changes in the readme file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {BasicHighlightDirective} from "./game/directives/basic-highlight/basic-highlight.directive";
+import {BetterHighlightDirective} from "./game/directives/better-highlight.directive";
+import {UnlessDirective} from "./game/directives/unless.directive";
+// import {GameControlComponent} from "./game/game-control/game-control.component";
+// import {OddComponent} from "./game/odd/odd.component";
+// import {EvenComponent} from "./game/even/even.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent, BasicHighlightDirective, BetterHighlightDirective, UnlessDirective, //These are for emitter practice
+    // GameControlComponent, OddComponent, EvenComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'udemy-angular-pracitce';
+  loadedFeature: string = 'recipe';
+
+  //This method is just created for emitter practice
+  // oddNumbers: number[] = [];
+  // evenNumbers: number[] = [];
+  // onIntervalFired(firedNumber: number) {
+  //   if (firedNumber % 2 == 0) {
+  //     this.evenNumbers.push(firedNumber)
+  //   } else {
+  //     this.oddNumbers.push(firedNumber)
+  //   }
+  // }
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
